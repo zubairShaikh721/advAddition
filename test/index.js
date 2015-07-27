116ea99103419090dfe1cd0166412505ac3d009b
zubairShaikh721 <shaikh.zubair721@hotmail.com>
2015-07-27 12:28:15 +0530

    mocha, chai
    chai

---
+++ test/index.js
@@ -0,0 +1,20 @@
+/*!
+ * Addition Trial
+ * Copyright(c) 22015-2016 TJ Holowaychuk
+ * Copyright(c) 2015 Roman Shtylman
+ * Copyright(c) 2015-2016 Zubair Lee Pranav
+ * Extentia Licensed
+ */
+
+'use strict';
+//module.exports = require('./require_module.js');
+
+//var add_module=require('./require_module');
+//add_module.advanceAddition(15,50);
+// advanceAddition(34,44);
+
+
+var should=require('chai').should(),
+advaddition=require('..index');
+
+advaddition.advanceAddition(23,30);
